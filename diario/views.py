3dafc8ed10061b1999d354397a9721cff17b5ca3
Hernando Tejedor <htejedor@gmail.com>
2020-06-05 19:16:48 -0300

    Primeros UT de diario. View home devuelve html válido

--- diario/views.py
+++ diario/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home():
-    pass
+def home(request):
+    return HttpResponse('<html><title>Finanzas Personales - Movimientos diarios</title></html>')
+
