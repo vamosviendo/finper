d24cb22c4b4ea5ea69e88eda5a33f3b50ad32b6c
Hernando Tejedor <htejedor@gmail.com>
2020-06-05 09:52:30 -0300

    FTs para comprobar titulo y encabezado

--- fts/test_escritura_simple.py
+++ fts/test_escritura_simple.py
@@ -5,12 +5,34 @@ from .base import FunctionalTest
 
 class TestVisitanteNuevo(FunctionalTest):
 
-    def test_puede_escribir_una_obra(self):
-        # Entro a la página web. El título es Drama - Editor online de textos
-        # dramáticos
+    def test_muestra_tabla_con_form_de_entrada(self):
+        # Entro a la página web. El título es "Finanzas personales"
         self.browser.get(self.live_server_url)
-        self.assertIn('Title', self.browser.title)
-        encabezado = self.browser.find_element_by_tag_name('h1')
-        self.assertIn('Encabezado', encabezado)
+        self.assertIn('Finanzas personales', self.browser.title)
+        encabezado = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Finanzas personales', encabezado)
 
+        # Debajo del encabezado hay una tabla con los siguientes encabezados:
+        # Fecha, concepto, detalle, entrada, salida, total
+
+        # Debajo del encabezado, las celdas de la tabla están ocupadas por
+        # campos de un formulario.
+        # Ingreso un movimiento con fecha, concepto, detalle, entrada y salida.
+        # La columna total se completa automáticamente con el cálculo del
+        # saldo anterior.
+
+        # Los datos ingresados pasan a formar parte del texto de la página,
+        # y el formulario se desplaza una columna hacia abajo.
+
+
+
+
+        # Al costado del total, un botón de Agregar Cuenta
+
+        # Pulso el botón de agregar cuenta, se abre una página con
+        # un formulario para agregar una nueva cuenta.
+
+        # Completo el formulario y le doy OK. Regreso a la página principal.
+        # Ésta tiene una nueva columna, cuyo encabezado es el nombre de la
+        # cuenta.
 
