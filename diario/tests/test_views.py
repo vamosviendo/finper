da624363a033b6f82b933eea42ba94842b97a057
vamosviendo <htejedor@gmail.com>
2021-04-14 19:22:06 -0300

    View home usa template

--- diario/tests/test_views.py
+++ diario/tests/test_views.py
@@ -9,3 +9,7 @@ class TestHomePage(TestCase):
     def test_resuelve_a_view_home(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
+
+    def test_usa_template_home(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'diario/home.html')
