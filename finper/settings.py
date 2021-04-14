6587fed53595375bfda6002765d548a19794329f
vamosviendo <htejedor@gmail.com>
2021-04-14 20:32:39 -0300

    Agregado (y desactivado) código para aumentar verbosidad de loggig

--- finper/settings.py
+++ finper/settings.py
@@ -124,3 +124,21 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'console': {
+#             'level': 'DEBUG',
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers': ['console'],
+#         },
+#     },
+#     'root': {'level': 'INFO'},
+# }
