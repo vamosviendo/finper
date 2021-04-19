6b4fb434934cf6bdd80902b07a09135c46c1ec81
vamosviendo <htejedor@gmail.com>
2021-04-19 12:00:43 -0300

    Modelo Movimiento. Agregados campos y validación de cuentas de entrada y salida

--- diario/models.py
+++ diario/models.py
@@ -1,9 +1,32 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
 class Cuenta(models.Model):
-    nombre = models.TextField(max_length=50)
+    nombre = models.CharField(max_length=50, unique=True)
 
 
 class Movimiento(models.Model):
-    pass
+    fecha = models.DateField()
+    concepto = models.CharField(max_length=80)
+    detalle = models.TextField(blank=True)
+    importe = models.FloatField()
+    cta_entrada = models.ForeignKey(
+        Cuenta, related_name='entradas', null=True, blank=True,
+        on_delete=models.CASCADE
+    )
+    cta_salida = models.ForeignKey(
+        Cuenta, related_name='salidas', null=True, blank=True,
+        on_delete=models.CASCADE
+    )
+
+    def clean(self):
+        super().clean()
+        if not self.cta_entrada and not self.cta_salida:
+            raise ValidationError(
+                message='Debe haber una cuenta de entrada, '
+                        'una de salida o ambas.'
+            )
+        if self.cta_entrada == self.cta_salida:
+            raise ValidationError(
+                message='Cuentas de entrada y salida no pueden ser la misma.')
--- diario/tests/test_models.py
+++ diario/tests/test_models.py
@@ -1,6 +1,8 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.utils.datetime_safe import date
 
-from diario.models import Cuenta
+from diario.models import Cuenta, Movimiento
 
 
 class TestModelCuenta(TestCase):
@@ -21,3 +23,124 @@ class TestModelCuenta(TestCase):
         segunda_cuenta_guardada = cuentas_guardadas[1]
         self.assertEqual(primera_cuenta_guardada.nombre, 'Efectivo')
         self.assertEqual(segunda_cuenta_guardada.nombre, 'Caja de ahorro')
+
+    def test_no_permite_cuentas_duplicadas(self):
+        Cuenta.objects.create(nombre='Efectivo')
+        with self.assertRaises(ValidationError):
+            cuenta2 = Cuenta(nombre='Efectivo')
+            cuenta2.full_clean()
+
+
+class TestModelMovimiento(TestCase):
+
+    def test_guarda_y_recupera_movimientos(self):
+        cuenta = Cuenta.objects.create(nombre='Efectivo')
+
+        primer_mov = Movimiento()
+        primer_mov.fecha = date.today()
+        primer_mov.concepto = 'entrada de efectivo'
+        primer_mov.importe = 985.5
+        primer_mov.cta_entrada = cuenta
+        primer_mov.save()
+
+        segundo_mov = Movimiento()
+        segundo_mov.fecha = date(2021, 4, 5)
+        segundo_mov.concepto = 'compra en efectivo'
+        segundo_mov.detalle = 'salchichas, pan, mostaza'
+        segundo_mov.importe = 500
+        segundo_mov.cta_salida = cuenta
+        segundo_mov.save()
+
+        movs_guardados = Movimiento.objects.all()
+        self.assertEqual(movs_guardados.count(), 2)
+
+        primer_mov_guardado = movs_guardados[0]
+        segundo_mov_guardado = movs_guardados[1]
+
+        self.assertEqual(primer_mov_guardado.fecha, date.today())
+        self.assertEqual(primer_mov_guardado.concepto, 'entrada de efectivo')
+        self.assertEqual(primer_mov_guardado.importe, 985.5)
+        self.assertEqual(primer_mov_guardado.cta_entrada, cuenta)
+
+        self.assertEqual(segundo_mov_guardado.fecha, date(2021, 4, 5))
+        self.assertEqual(segundo_mov_guardado.concepto, 'compra en efectivo')
+        self.assertEqual(
+            segundo_mov_guardado.detalle, 'salchichas, pan, mostaza')
+        self.assertEqual(segundo_mov_guardado.importe, 500)
+        self.assertEqual(segundo_mov_guardado.cta_salida, cuenta)
+
+    def test_permite_movimientos_duplicados(self):
+        cuenta = Cuenta.objects.create(nombre='Efectivo')
+        Movimiento.objects.create(
+            fecha=date.today(),
+            concepto='Cobranza en efectivo',
+            importe=100,
+            cta_entrada=cuenta
+        )
+        mov = Movimiento(
+            fecha=date.today(),
+            concepto='Cobranza en efectivo',
+            importe=100,
+            cta_entrada=cuenta
+        )
+        mov.full_clean()    # No debe dar error
+
+    def test_cta_entrada_se_relaciona_con_cuenta(self):
+        cuenta = Cuenta.objects.create(nombre='Efectivo')
+        mov = Movimiento(
+            fecha=date.today(), concepto='Cobranza en efectivo', importe=100)
+        mov.cta_entrada = cuenta
+        mov.save()
+        self.assertIn(mov, cuenta.entradas.all())
+
+    def test_cta_salida_se_relaciona_con_cuenta(self):
+        cuenta = Cuenta.objects.create(nombre='Efectivo')
+        mov = Movimiento(
+            fecha=date.today(), concepto='Cobranza en efectivo', importe=100)
+        mov.cta_salida = cuenta
+        mov.save()
+        self.assertIn(mov, cuenta.salidas.all())
+
+    def test_permite_guardar_cuentas_de_entrada_y_salida_en_un_movimiento(self):
+        cuenta1 = Cuenta.objects.create(nombre='Efectivo')
+        cuenta2 = Cuenta.objects.create(nombre='Banco')
+        mov = Movimiento(
+            fecha=date.today(),
+            concepto='Retiro de efectivo',
+            importe=100,
+            cta_entrada=cuenta1,
+            cta_salida=cuenta2
+        )
+
+        mov.full_clean()    # No debe dar error
+        mov.save()
+
+        self.assertIn(mov, cuenta1.entradas.all())
+        self.assertIn(mov, cuenta2.salidas.all())
+        self.assertNotIn(mov, cuenta1.salidas.all())
+        self.assertNotIn(mov, cuenta2.entradas.all())
+
+    def test_requiere_al_menos_una_cuenta(self):
+        mov = Movimiento(
+            fecha=date.today(),
+            concepto='Cobranza en efectivo',
+            importe=100
+        )
+        with self.assertRaisesMessage(
+                ValidationError,
+                'Debe haber una cuenta de entrada, una de salida o ambas.'
+        ):
+            mov.full_clean()
+
+    def test_no_admite_misma_cuenta_de_entrada_y_de_salida(self):
+        cuenta = Cuenta.objects.create(nombre='Efectivo')
+        mov = Movimiento(
+            fecha=date.today(),
+            concepto='Cobranza en efectivo',
+            importe=100,
+            cta_entrada=cuenta,
+            cta_salida=cuenta
+        )
+        with self.assertRaisesMessage(
+                ValidationError, 'Cuentas de entrada y salida no pueden ser la misma.'):
+            mov.full_clean()
