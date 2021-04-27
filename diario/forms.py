1a605d0af746656121fc7eee53fb32b8f36cbdd0
vamosviendo <htejedor@gmail.com>
2021-04-27 19:21:41 -0300

    Aplicado diseño inicial a páginas de carga de cuentas y movimientos

--- diario/forms.py
+++ diario/forms.py
@@ -5,12 +5,24 @@ from diario.models import Cuenta, Movimiento
 
 class FormCuenta(ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['nombre'].widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Cuenta
         fields = ('nombre', )
 
 
 class FormMovimiento(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for nombre, campo in self.fields.items():
+            if campo.widget.attrs.get('class'):
+                campo.widget.attrs['class'] += ' form-control'
+            else:
+                campo.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Movimiento
