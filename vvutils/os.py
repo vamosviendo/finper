a48029aeb3ca76b1b54b08e6c0cc99f321680080
vamosviendo <htejedor@gmail.com>
2022-02-11 09:58:46 -0300

    Agregado módulo vvutils

---
+++ vvutils/os.py
@@ -0,0 +1,18 @@
+import os
+from getpass import getpass
+
+
+def env_or_input(env_var, msj=None, password=False):
+    """ Busca una variable en el entorno. Si no la encuentra, la pide.
+        password=True: oculta el texto tipeado.
+    """
+    msj = msj or f'Ingrese valor para {env_var}: '
+    try:
+        result = os.environ[env_var]
+    except KeyError:
+        if password:
+            result = getpass(msj)
+        else:
+            result = input(msj)
+
+    return result
