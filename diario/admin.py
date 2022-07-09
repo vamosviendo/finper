c4a12de10610a78e50f37eea614293fbd40aca28
vamosviendo <htejedor@gmail.com>
2022-07-09 11:40:57 -0300

    Modelo Movimiento registrado en admin

--- diario/admin.py
+++ diario/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from diario.models import Movimiento
+
+
+class MovimientoAdmin(admin.ModelAdmin):
+    fields = [
+        'fecha', 'orden_dia', 'concepto', 'detalle', '_importe',
+        'cta_entrada', 'cta_salida', 'id_contramov',
+        'convierte_cuenta', 'es_automatico'
+    ]
+
+
+admin.site.register(Movimiento, MovimientoAdmin)
