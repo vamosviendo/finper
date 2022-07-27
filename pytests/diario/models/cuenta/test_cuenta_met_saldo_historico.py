42dbe3d86d9f6f90551aba83dcbf13017325b244
vamosviendo <htejedor@gmail.com>
2022-07-26 22:16:37 -0300

    Agregados tests de pytest para método Cuenta.saldo_historico

---
+++ pytests/diario/models/cuenta/test_cuenta_met_saldo_historico.py
@@ -0,0 +1,17 @@
+import pytest
+
+from diario.models import Saldo
+
+pytestmark = pytest.mark.django_db
+
+
+def test_recupera_saldo_al_momento_del_movimiento(cuenta, entrada, traspaso_posterior, entrada_tardia, mocker):
+    mock_tomar = mocker.patch('diario.models.cuenta.Saldo.tomar')
+    cuenta.saldo_historico(entrada)
+    mock_tomar.assert_called_once_with(cuenta=cuenta, movimiento=entrada)
+
+
+def test_si_no_encuentra_saldo_de_cuenta_en_fecha_de_mov_devuelve_0(cuenta, entrada, mocker):
+    mock_tomar = mocker.patch('diario.models.cuenta.Saldo.tomar')
+    mock_tomar.side_effect = Saldo.DoesNotExist
+    assert cuenta.saldo_historico(entrada) == 0
