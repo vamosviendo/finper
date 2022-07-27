169d12d4e00fa3339c0b478863c80b52b76f7678
vamosviendo <htejedor@gmail.com>
2022-07-26 22:01:36 -0300

    Agregados tests de pytest para método Cuenta.ancestros

---
+++ pytests/diario/models/cuenta/test_cuenta_met_ancestros.py
@@ -0,0 +1,25 @@
+import pytest
+
+from diario.models import CuentaInteractiva
+
+pytestmark = pytest.mark.django_db
+
+
+def test_devuelve_lista_de_todas_las_cuentas_ancestro(cuenta: CuentaInteractiva):
+    subc1, subc2 = cuenta.dividir_entre(
+        ['subcuenta 1', 'sc1', 0],
+        ['subcuenta 2', 'sc2']
+    )
+    subsubc1, subsubc2 = subc1.dividir_entre(
+        ['subsubcuenta 1', 'ssc1', 0],
+        ['subsubcuenta 2', 'ssc2']
+    )
+    subsubsubc1, subsubsubc2 = subsubc1.dividir_entre(
+        ['subsubsubcuenta 1', 'sssc1', 0],
+        ['subsubsubcuenta 2', 'sssc2']
+    )
+    subsubc1 = subsubc1.tomar_del_slug()
+    subc1 = subc1.tomar_del_slug()
+    cta = cuenta.tomar_del_slug()
+
+    assert subsubsubc1.ancestros() == [subsubc1, subc1, cta]
