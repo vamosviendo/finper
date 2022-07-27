26180538ea4ef6cf482fd92bca26ec4903184603
vamosviendo <htejedor@gmail.com>
2022-07-26 21:55:50 -0300

    Agregados tests de pytest para método Cuenta.hermanas

---
+++ pytests/diario/models/cuenta/test_cuenta_met_hermanas.py
@@ -0,0 +1,27 @@
+import pytest
+
+from diario.models import CuentaInteractiva
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def subcuentas(cuenta: CuentaInteractiva):
+    return cuenta.dividir_entre(
+        {'nombre': 'subcuenta 1', 'slug': 'sc1', 'saldo': 30, },
+        {'nombre': 'subcuenta 2', 'slug': 'sc2', 'saldo': 1},
+        {'nombre': 'subcuenta 3', 'slug': 'sc3'}
+    )
+
+
+def test_devuelve_hijas_de_la_misma_madre(subcuentas):
+    for subc in subcuentas[1:]:
+        assert subc in subcuentas[0].hermanas()
+
+
+def test_cuenta_no_se_incluye_a_si_misma_entre_sus_hermanas(subcuentas):
+    assert subcuentas[0] not in subcuentas[0].hermanas()
+
+
+def test_devuelve_none_si_cuenta_no_tiene_madre(cuenta):
+    assert cuenta.hermanas() is None
