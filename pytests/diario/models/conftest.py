ec56cbc98f97cb5c97b9f16a741ee9b959628936
vamosviendo <htejedor@gmail.com>
2022-08-08 10:32:10 -0300

    Agregados tests de pytest para métodos CuentaAcumulativa.movs_conversion y CuentaAcumulativa.movs_no_conversion

--- pytests/diario/models/conftest.py
+++ pytests/diario/models/conftest.py
@@ -1,6 +1,18 @@
+from __future__ import annotations
+
+from typing import List, Dict
+
 import pytest
 
 
 def pytest_collection_modifyitems(items):
     for item in items:
         item.add_marker(pytest.mark.django_db)
+
+
+@pytest.fixture
+def dict_subcuentas() -> List[Dict[str, str | int]]:
+    return [
+        {'nombre': 'Billetera', 'slug': 'ebil', 'saldo': 50},
+        {'nombre': 'Cajón de arriba', 'slug': 'ecaj'},
+    ]
