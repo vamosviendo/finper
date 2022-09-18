c99f20c6674324f0ce0184a14d7697983904828e
vamosviendo <htejedor@gmail.com>
2022-09-18 10:00:53 -0300

    Agregados tests de pytest para view CtaElimView

---
+++ pytests/diario/views/test_views_cta_elim.py
@@ -0,0 +1,27 @@
+from django.urls import reverse
+from pytest_django import asserts
+
+from diario.models import Cuenta
+
+
+def test_get_usa_template_cuenta_confirm_delete(client, cuenta):
+    response = client.get(reverse('cta_elim', args=[cuenta.slug])
+    )
+    asserts.assertTemplateUsed(response, 'diario/cuenta_confirm_delete.html')
+
+
+def test_post_elimina_cuenta(client, cuenta):
+    cantidad = Cuenta.cantidad()
+    client.post(reverse('cta_elim', args=[cuenta.slug]))
+    assert Cuenta.cantidad() == cantidad - 1
+
+
+def test_redirige_a_home_despues_de_borrar(client, cuenta):
+    response = client.post(reverse('cta_elim', args=[cuenta.slug])
+    )
+    asserts.assertRedirects(response, reverse('home'))
+
+
+def test_muestra_mensaje_de_error_si_se_elimina_cuenta_con_saldo(client, cuenta_con_saldo):
+    response = client.get(reverse('cta_elim', args=[cuenta_con_saldo.slug]))
+    asserts.assertContains(response, 'No se puede eliminar cuenta con saldo')
