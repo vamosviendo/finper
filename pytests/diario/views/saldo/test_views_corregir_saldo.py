845de738eb3f5ac179dea8142cf78e36f8253aa3
vamosviendo <htejedor@gmail.com>
2022-09-21 20:17:12 -0300

    Agregados tests de pytest para view CorregirSaldo

---
+++ pytests/diario/views/saldo/test_views_corregir_saldo.py
@@ -0,0 +1,32 @@
+import pytest
+from django.urls import reverse
+from pytest_django import asserts
+
+from diario.models import Cuenta
+
+
+@pytest.fixture
+def full_url(cuenta: Cuenta, cuenta_2: Cuenta) -> str:
+    return f"{reverse('corregir_saldo')}?ctas={cuenta.slug}!{cuenta_2.slug}"
+
+
+def test_usa_template_corregir_saldo(client, full_url):
+    response = client.get(full_url)
+    asserts.assertTemplateUsed(response, 'diario/corregir_saldo.html')
+
+
+def test_redirige_a_home_si_no_recibe_querystring_o_con_querystring_mal_formada(client):
+    url1 = reverse('corregir_saldo')
+    url2 = f"{reverse('corregir_saldo')}?ctas="
+    url3 = f"{reverse('corregir_saldo')}?ctas=a"
+    url4 = f"{reverse('corregir_saldo')}?cuculo=2"
+    asserts.assertRedirects(client.get(url1), reverse('home'))
+    asserts.assertRedirects(client.get(url2), reverse('home'))
+    asserts.assertRedirects(client.get(url3), reverse('home'))
+    asserts.assertRedirects(client.get(url4), reverse('home'))
+
+
+def test_pasa_lista_de_cuentas_erroneas_a_template(
+        client, full_url, cuenta, cuenta_2, cuenta_3):
+    response = client.get(full_url)
+    assert response.context['ctas_erroneas'] == [cuenta, cuenta_2]
