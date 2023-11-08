e2cd209c7a7609ad9a116c0d3ad50757ce6a9a20
Hernando Tejedor en arch <htejedor@gmail.com>
2023-11-08 13:25:36 -0300

    Testeo de datos de moneda mostrados en páginas principal pasados de test_crear_moneda a test_home

--- pytests/functional/test_crear_modificar_eliminar_moneda.py
+++ pytests/functional/test_crear_modificar_eliminar_moneda.py
@@ -17,45 +17,12 @@ def test_crear_moneda(browser, cuenta_con_saldo, cuenta_con_saldo_negativo, cuen
     browser.assert_url(reverse('home'))
 
     # Vemos que la moneda creada aparece entre las monedas de la página de
-    # inicio
+    # inicio, con su cotización al lado
     links_moneda = browser.esperar_elementos("class_link_moneda")
     nombres_moneda = [x.text.strip() for x in links_moneda]
     assert "moneda nueva" in nombres_moneda
-
-    # Vemos que al lado de la moneda aparece la cotización de la misma
     cot_moneda = browser.esperar_elemento("id_cotizacion_mn")
     assert cot_moneda.text == float_format(15)
-
-    # Vemos que en la sección de saldos aparecen los saldos generales en distintas
-    # monedas, así como una columna para el saldo total expresado en cada una de
-    # las monedas existentes.
-    saldo_general = sum(c.saldo for c in Cuenta.filtro(cta_madre=None))
-    saldo_en_moneda_nueva = browser.esperar_elemento("id_saldo_gral_mn")
-    assert saldo_en_moneda_nueva.text == '0,00'
-    saldo_expresado_en_moneda_nueva = browser.esperar_elemento("id_saldo_gral_expresado_en_mn")
-    assert saldo_expresado_en_moneda_nueva == float_format(saldo_general / 15)
-
-    # Vemos que al lado de cada cuenta aparece una columna con su saldo expresado
-    # en la nueva moneda, en una tipografía más clara que la de su moneda original
-    for cuenta in Cuenta.todes():
-        saldo_mon_nueva = browser.esperar_elemento(f"id_saldo_cta_{cuenta.slug}_en_mn")
-        assert saldo_mon_nueva.text == float_format(cuenta.saldo / 15)
-
-    # Vemos que al lado de cada titular aparece una columna para su capital
-    # en la nueva moneda, así como una columna para el capital total expresado
-    # en cada una de las monedas existentes
-    for titular in Titular.todes():
-        capital_en_mon_nueva = browser.esperar_elemento(f"id_capital_{titular.titname}_en_mn")
-        assert capital_en_mon_nueva.text == "0.00"
-        capital_expresado_en_mon_nueva = titular.esperar_elemento(f"id_capital_{titular.titname}_expresado_en_mn")
-        assert capital_expresado_en_mon_nueva.text == float_format(titular.capital / 15)
-
-    # Vemos que en la sección de movimientos aparece una columna para el importe
-    # de movimientos hechos en la nueva moneda
-    browser.esperar_elemento("id_th_importe_mn")
-    for movimiento in Movimiento.todes():
-        importe_en_mon_nueva = browser.esperar_elemento("class_td_importe_mn")
-        assert importe_en_mon_nueva.text == '0.00'
 
 
 def test_modificar_moneda(browser):
