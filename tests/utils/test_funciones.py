0df9ca7da913c31c19a52c5bad2eb5b98ebb7629
Hernando Tejedor en arch <htejedor@gmail.com>
2023-11-15 18:12:19 -0300

    Función utils.numeros.format_float convierte str de número con coma en float

--- tests/utils/test_funciones.py
+++ tests/utils/test_funciones.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from unittest import TestCase
 
 from utils.archivos import fijar_mtime
-from utils.numeros import float_or_none, float_format
+from utils.numeros import float_or_none, float_format, format_float
 
 
 class TestFijarTimestamp(TestCase):
@@ -38,7 +38,7 @@ class TestFloatOrNone(TestCase):
         self.assertEqual(float_or_none((2, 3)), None)
 
 
-class TestFloatStrComa(TestCase):
+class TestFloatFormat(TestCase):
 
     def test_devuelve_float_en_forma_de_str_con_coma(self):
         self.assertEqual(float_format(2.25), "2,25")
@@ -47,3 +47,10 @@ class TestFloatStrComa(TestCase):
         self.assertEqual(float_format(2.2), "2,20")
         self.assertEqual(float_format(2.256), "2,26")
         self.assertEqual(float_format(2), "2,00")
+
+
+class TestFormatFloat:    # Inversa de float_format()
+    def test_devuelve_str_de_numeros_con_coma_como_float(self):
+        assert format_float("2,25") == 2.25
+        assert format_float('2.25') == 2.25
+        assert format_float('2') == 2.0
--- utils/numeros.py
+++ utils/numeros.py
@@ -14,3 +14,7 @@ def float_or_none(valor: Any) -> float | None:
 
 def float_format(num: str | float, lugares: int = 2) -> str:
     return number_format(round(float(num), lugares), lugares)
+
+
+def format_float(num: str) -> float:
+    return float(num.replace(',', '.'))
