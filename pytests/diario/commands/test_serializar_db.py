197e84832c4c7862d25679507a34c000ba246d95
Hernando Tejedor en arch <htejedor@gmail.com>
2024-03-17 20:14:10 -0300

    Comando actualizar_db guarda todos los días y movimientos de la base de datos en la serialización

--- pytests/diario/commands/test_serializar_db.py
+++ pytests/diario/commands/test_serializar_db.py
@@ -5,21 +5,14 @@ import pytest
 from django.apps import apps
 from django.core.management import call_command
 
+from diario.serializers import MovimientoSerializado
 from utils.archivos import es_json_valido
-from vvmodel.serializers import load_serialized_filename
 
 
 @pytest.fixture(autouse=True)
 def borrar_db_full():
     yield
     Path('db_full.json').unlink(missing_ok=True)
-
-
-@pytest.fixture
-def db_serializada():
-    call_command('serializar_db')
-    yield load_serialized_filename("db_full.json")
-    Path('db_full.json').unlink()
 
 
 @pytest.fixture
@@ -31,6 +24,7 @@ def varios_titulares(titular, otro_titular, titular_gordo):
 def varias_monedas(peso, dolar, euro):
     return [peso, dolar, euro]
 
+
 @pytest.fixture
 def varias_cuentas(
         cuenta, cuenta_2, cuenta_3, cuenta_ajena, cuenta_acumulativa,
@@ -39,6 +33,23 @@ def varias_cuentas(
         cuenta, cuenta_2, cuenta_3, cuenta_ajena, cuenta_acumulativa,
         cuenta_acumulativa_en_dolares, cuenta_en_dolares
     ]
+
+
+@pytest.fixture
+def varios_movimientos(
+        entrada, salida, entrada_tardia, traspaso, traspaso_posterior, credito,
+        entrada_temprana, entrada_cuenta_ajena, entrada_en_dolares,
+        salida_tardia_tercera_cuenta):
+    return [
+        entrada, salida, entrada_tardia, traspaso, traspaso_posterior, credito,
+        entrada_temprana, entrada_cuenta_ajena, entrada_en_dolares,
+        salida_tardia_tercera_cuenta
+    ]
+
+
+@pytest.fixture
+def varios_dias(dia_temprano, dia_anterior, dia, dia_posterior, dia_tardio, dia_tardio_plus):
+    return [dia_temprano, dia_anterior, dia, dia_posterior, dia_tardio, dia_tardio_plus]
 
 
 def test_genera_archivo_db_full_json():
@@ -53,26 +64,26 @@ def test_archivo_generado_es_json_valido():
         assert es_json_valido(db_full)
 
 
-@pytest.mark.parametrize("modelo, elementos, identificador", [
-    ("titular", "varios_titulares", "titname"),
-    ("moneda", "varias_monedas", "monname"),
-    ("cuenta", "varias_cuentas", "slug"),
-    # ("cuentainteractiva", "varias_cuentas", "slug"),
-    # ("cuentaacumulativa", "varias_cuentas", "slug"),
+@pytest.mark.parametrize("modelo, elementos, identificador, key", [
+    ("titular", "varios_titulares", "titname", None),
+    ("moneda", "varias_monedas", "monname", None),
+    ("cuenta", "varias_cuentas", "slug", None),
+    ("dia", "varios_dias", "identidad", "fecha"),
 ])
-def test_serializa_todos_los_elementos_de_diario_en_json(modelo, elementos, identificador, request):
+def test_serializa_todos_los_titulares_monedas_cuentas_y_dias_de_la_base_de_datos_en_json(modelo, elementos, identificador, key, request):
+    key = key or identificador
     elementos = request.getfixturevalue(elementos)
     db_serializada = request.getfixturevalue("db_serializada")
     elementos_ser = db_serializada.filter_by_model("diario", modelo)
     assert len(elementos_ser) == apps.get_model("diario", modelo).cantidad()
     for elem in elementos:
         assert getattr(elem, identificador) in [
-            es.fields[identificador] for es in elementos_ser
+            es.fields[key] for es in elementos_ser
         ]
 
 
 @pytest.mark.parametrize("modelo", ["cuentainteractiva", "cuentaacumulativa"])
-def test_serializa_todas_las_cuentas_interactivas(modelo, varias_cuentas, db_serializada):
+def test_serializa_todas_las_cuentas_interactivas_y_acumulativas(modelo, varias_cuentas, db_serializada):
     cuentas_ser = db_serializada.filter_by_model("diario", "cuenta")
     cuentas_subc_ser = db_serializada.filter_by_model("diario", modelo)
     for cuenta_int in cuentas_subc_ser:
@@ -81,3 +92,19 @@ def test_serializa_todas_las_cuentas_interactivas(modelo, varias_cuentas, db_ser
     assert len(cuentas_subc_ser) == clase_modelo.cantidad()
     for ci in clase_modelo.todes():
         assert ci.slug in [cis.fields['slug'] for cis in cuentas_subc_ser]
+
+
+def test_serializa_todos_los_movimientos(varios_movimientos, db_serializada):
+    movimientos_ser = [
+        MovimientoSerializado(x) for x in db_serializada.filter_by_model("diario", "movimiento")
+    ]
+    Movimiento = apps.get_model("diario", "movimiento")
+    assert len(movimientos_ser) == Movimiento.cantidad()
+
+    identidades = [d.identidad for d in movimientos_ser]
+    for mov in Movimiento.todes():
+        assert mov.identidad in identidades
+
+@pytest.mark.xfail
+def test_serializa_todos_los_saldos(varios_saldos, db_serializada):
+    pytest.fail('escribir')
