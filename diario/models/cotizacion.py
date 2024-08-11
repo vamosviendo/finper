4e05f4d1082e2315489b169c08e0357fd15d255e
Hernando Tejedor en arch <htejedor@gmail.com>
2024-08-11 20:47:35 -0300

    Método de clase Cotizacion.tomar() devuelve cotización de una moneda en la fecha dada o en la última fecha anterior

--- diario/models/cotizacion.py
+++ diario/models/cotizacion.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.db import models
 
 from vvmodel.models import MiModel
@@ -11,3 +13,18 @@ class Cotizacion(MiModel):
     class Meta:
         ordering = ("fecha", )
         unique_together = ("fecha", "moneda", )
+
+    @classmethod
+    def tomar(cls, **kwargs):
+        kwargs["fecha"] = kwargs.get("fecha") or date.today()
+
+        if "moneda" not in kwargs.keys():
+            raise TypeError('Argumento "moneda" faltante')
+        for key in kwargs.keys():
+            if key not in ("moneda", "fecha"):
+                raise TypeError(f'Argumento "{key}" inesperado')
+
+        try:
+            return super().tomar(**kwargs)
+        except cls.DoesNotExist:
+            return cls.filtro(moneda=kwargs["moneda"], fecha__lt=kwargs["fecha"]).last()
---
+++ pytests/diario/models/cotizacion/test_cot_tomar.py
@@ -0,0 +1,29 @@
+import pytest
+
+from diario.models import Cotizacion
+
+
+def test_devuelve_cotizacion_de_una_moneda_en_una_fecha(
+        cotizacion, cotizacion_posterior, dolar, fecha_posterior):
+    assert Cotizacion.tomar(moneda=dolar, fecha=fecha_posterior) == cotizacion_posterior
+
+
+def test_si_no_encuentra_cotizacion_de_moneda_en_fecha_devuelve_ultima_cotizacion_anterior(
+        cotizacion, cotizacion_posterior, dolar, fecha_tardia):
+    assert Cotizacion.tomar(moneda=dolar, fecha=fecha_tardia) == cotizacion_posterior
+
+
+def test_toma_fecha_de_hoy_por_defecto(cotizacion, dolar, mock_today):
+    hoy = mock_today.return_value
+    cotizacion_actual = Cotizacion.crear(moneda=dolar, fecha=hoy, importe=255)
+    assert Cotizacion.tomar(moneda=dolar) == cotizacion_actual
+
+
+def test_si_no_recibe_moneda_da_error(cotizacion, fecha):
+    with pytest.raises(TypeError):
+        Cotizacion.tomar(fecha=fecha)
+
+
+def test_si_recibe_argumento_distinto_a_fecha_o_moneda_da_error(cotizacion, cotizacion_posterior, dolar, fecha):
+    with pytest.raises(TypeError):
+        Cotizacion.tomar(moneda=dolar, importe=cotizacion.importe)
