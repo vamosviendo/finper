32e5bc975657a1b850b0038557c39e0c390683ca
Hernando Tejedor en arch <htejedor@gmail.com>
2024-08-16 20:55:08 -0300

    Test adaptado a campo Movimiento._cotizacion

--- pytests/diario/models/cuenta/test_c_met_total_movs.py
+++ pytests/diario/models/cuenta/test_c_met_total_movs.py
@@ -20,11 +20,14 @@ def test_funciona_correctamente_con_decimales(cuenta):
     assert cuenta.total_movs() == round(110 - 50.32, 2)
 
 
-def test_computa_correctamente_movimientos_en_moneda_distinta_a_la_de_la_cuenta(cuenta, cuenta_en_dolares, dolar, peso):
+def test_computa_correctamente_movimientos_en_moneda_distinta_a_la_de_la_cuenta(
+        cuenta, cuenta_en_dolares, dolar, peso, fecha):
     Movimiento.crear(
         "Compra de dólares expresada en pesos",
+        fecha=fecha,
         cta_entrada=cuenta_en_dolares,
         cta_salida=cuenta,
         importe=850, moneda=peso,
+        cotizacion=1.0 / dolar.cotizacion_al(fecha)
     )
-    assert cuenta_en_dolares.total_movs(), 2 == round(850 * peso.cotizacion_en(dolar), 2)
+    assert cuenta_en_dolares.total_movs() == round(850 * peso.cotizacion_en(dolar), 2)
