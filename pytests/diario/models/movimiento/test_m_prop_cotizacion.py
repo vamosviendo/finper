31b22d3a137f6555882f0a5baf85fc4ff9eb949d
Hernando Tejedor en arch <htejedor@gmail.com>
2024-08-15 21:12:18 -0300

    Ajustados tests de la prop Movimiento.cotizacion

--- pytests/diario/models/movimiento/test_m_prop_cotizacion.py
+++ pytests/diario/models/movimiento/test_m_prop_cotizacion.py
@@ -1,7 +1,12 @@
 import pytest
 
 
-@pytest.mark.parametrize('mov', ['entrada_en_dolares', 'entrada_en_euros'])
-def test_devuelve_cotizacion_de_la_moneda_de_la_cuenta(mov, request):
+@pytest.mark.parametrize('mov', ['entrada_en_dolares', 'entrada_en_euros', 'mov_distintas_monedas'])
+def test_devuelve_contenido_del_campo__cotizacion(mov, request):
     movimiento = request.getfixturevalue(mov)
-    assert movimiento.cotizacion == movimiento.moneda.cotizacion
+    assert movimiento.cotizacion == movimiento._cotizacion
+
+
+def test_fija_contenido_del_campo__cotizacion(mov_distintas_monedas):
+    mov_distintas_monedas.cotizacion = 184
+    assert mov_distintas_monedas._cotizacion == 184
