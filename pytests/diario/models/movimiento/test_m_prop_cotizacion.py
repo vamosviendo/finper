44e28e7ad9e9300f1c84aacf1b93fdb41da43ef3
Hernando Tejedor en arch <htejedor@gmail.com>
2025-02-03 20:45:57 -0300

    Tests de prop Movimiento.cotizacion adaptados a nuevos fixtures de movimiento

--- pytests/diario/models/movimiento/test_m_prop_cotizacion.py
+++ pytests/diario/models/movimiento/test_m_prop_cotizacion.py
@@ -1,12 +1,20 @@
 import pytest
 
 
-@pytest.mark.parametrize('mov', ['entrada_en_dolares', 'entrada_en_euros', 'mov_distintas_monedas'])
+@pytest.mark.parametrize(
+    'mov', [
+        'entrada_en_dolares',
+        'entrada_en_euros',
+        'mov_distintas_monedas_en_moneda_cta_entrada',
+        'mov_distintas_monedas_en_moneda_cta_salida'
+    ])
 def test_devuelve_contenido_del_campo__cotizacion(mov, request):
     movimiento = request.getfixturevalue(mov)
     assert movimiento.cotizacion == movimiento._cotizacion
 
 
-def test_fija_contenido_del_campo__cotizacion(mov_distintas_monedas):
-    mov_distintas_monedas.cotizacion = 184
-    assert mov_distintas_monedas._cotizacion == 184
+@pytest.mark.parametrize("sentido", ["entrada", "salida"])
+def test_fija_contenido_del_campo__cotizacion(sentido, request):
+    movimiento = request.getfixturevalue(f"mov_distintas_monedas_en_moneda_cta_{sentido}")
+    movimiento.cotizacion = 184
+    assert movimiento._cotizacion == 184
