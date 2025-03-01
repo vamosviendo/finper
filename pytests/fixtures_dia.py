6dcc350a828aa220d2e7a4d0fbfe8064184c852d
Hernando Tejedor en arch <htejedor@gmail.com>
2025-03-01 18:58:07 -0300

    Agregados type hints a fixture fixtures_dias.mas_de_7_dias

--- pytests/fixtures_dia.py
+++ pytests/fixtures_dia.py
@@ -2,6 +2,7 @@ from datetime import date
 
 import pytest
 from django.core.exceptions import ValidationError
+from django.db.models import QuerySet
 
 from diario.models import Dia, Movimiento, Cuenta, Titular
 
@@ -66,13 +67,13 @@ def dia_hoy() -> Dia:
 
 @pytest.fixture
 def mas_de_7_dias(
-        dia_con_movs,
-        dia_temprano,
-        dia_tardio_con_movs,
-        dia_posterior_con_movs,
-        dia_anterior_con_movs,
-        dia_tardio_plus,
-        dia_hoy):
+        dia_con_movs: Dia,
+        dia_temprano: Dia,
+        dia_tardio_con_movs: Dia,
+        dia_posterior_con_movs: Dia,
+        dia_anterior_con_movs: Dia,
+        dia_tardio_plus: Dia,
+        dia_hoy: Dia) -> QuerySet[Dia]:
     titular = Titular.crear(nombre="titular_creado", titname="titc", fecha_alta=date(2001, 1, 2))
     cuenta = Cuenta.crear("cuenta_creada", "ccr", titular=titular, fecha_creacion=date(2001, 1, 2))
     Movimiento.crear(fecha=dia_temprano.fecha, concepto="mov", cta_entrada=cuenta, importe=100)
