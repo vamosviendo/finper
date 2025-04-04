155d0b932dcda3b548950a93f6782958faa67116
Hernando Tejedor en arch <htejedor@gmail.com>
2025-04-03 21:42:13 -0300

    Fixture db_serializada corregido para que al ejecutarse no elimine un archivo de serialización existente en caso de que exista

--- pytests/fixtures_serial.py
+++ pytests/fixtures_serial.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import Generator, Any
 
 import pytest
 
@@ -7,17 +8,30 @@ from django.apps import apps
 from django.core.management import call_command
 
 from diario.models import Movimiento
-from vvmodel.serializers import load_serialized_filename, SerializedDb
+from vvmodel.serializers import load_serialized_filename, SerializedDb, SerializedObject
 
 
 @pytest.fixture
-def db_serializada() -> SerializedDb:
+def db_serializada() -> Generator[SerializedDb, Any, None]:
+    # Proteger archivo de base de datos serializada si existe
+    db_full = Path("db_full.json")
+    if db_full.exists():
+        db_full.rename("db_full_backup.json")
+
     call_command('serializar_db')
     yield load_serialized_filename("db_full.json")
+
     # Descomentar para guardar una copia de la base de datos serializada:
     # import shutil
     # shutil.copyfile("db_full.json", "db_test.json")
-    Path('db_full.json').unlink()
+
+    # Reponer archivo de base de datos serializada. Si no existe,
+    # eliminar el generado por el fixture.
+    db_backup = Path("db_full_backup.json")
+    if db_backup.exists():
+        db_backup.rename("db_full.json")
+    else:
+        db_full.unlink()
 
 
 @pytest.fixture
