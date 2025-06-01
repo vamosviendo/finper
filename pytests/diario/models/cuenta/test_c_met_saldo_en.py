5dc4ca9d8e06b740358e27ba65345d2c00908742
Hernando Tejedor en arch <htejedor@gmail.com>
2025-06-01 20:47:40 -0300

    Retirados tests obsoletos

--- pytests/diario/models/cuenta/test_c_met_saldo_en.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.mark.parametrize("tipo", ["compra", "venta"])
-def test_devuelve_saldo_en_moneda_dada_redondeado_en_2_decimales(tipo, cuenta_con_saldo, peso, dolar):
-    compra = tipo == "compra"
-    assert \
-        cuenta_con_saldo.saldo(moneda=dolar, compra=compra) == \
-        round(cuenta_con_saldo.saldo() * cuenta_con_saldo.moneda.cotizacion_en(dolar, compra=compra), 2)
-    assert cuenta_con_saldo.saldo(moneda=peso, compra=compra) == cuenta_con_saldo.saldo()
