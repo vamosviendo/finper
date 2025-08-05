101496fadfd412e9605e89e80c486560c470548b
Hernando Tejedor en arch <htejedor@gmail.com>
2025-08-05 20:16:50 -0300

    v2.2.18.0 templatetag saldo muestra saldos generales históricos, saldos de cuentas o capitales de titulares en días o movimientos dados y en distintas monedas

--- diario/templatetags/historicos.py
+++ diario/templatetags/historicos.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from django import template
 
-from diario.models import Movimiento, Cuenta, Titular, Moneda
+from diario.models import Cuenta, Dia, Moneda, Movimiento, Titular
+from diario.utils.utils_saldo import saldo_general_historico
 from utils.numeros import float_format
 
 register = template.Library()
@@ -16,3 +17,32 @@ def cap_historico(titular: Titular, mov: Movimiento | None) -> str:
 @register.simple_tag
 def saldo_en_moneda(cuenta: Cuenta, moneda: Moneda, mov: Movimiento | None) -> str:
     return float_format(cuenta.saldo(movimiento=mov, moneda=moneda, compra=False))
+
+
+@register.simple_tag
+def saldo(
+        dia: Dia | None = None,
+        movimiento: Movimiento | None = None,
+        cuenta: Cuenta | None = None,
+        titular: Titular | None = None,
+        moneda: Moneda | None = None,
+) -> float:
+    dia = dia or Dia.ultime()
+    cotizacion = moneda.cotizacion_al(dia.fecha, compra=False) if moneda else 1
+
+    if cuenta:
+        if movimiento:
+            result = cuenta.saldo(movimiento=movimiento)
+        else:
+            result = cuenta.saldo(dia=dia)
+    elif titular:
+        if movimiento:
+            result = titular.capital(movimiento=movimiento)
+        else:
+            result = titular.capital(dia=dia)
+    elif movimiento:
+        result = saldo_general_historico(movimiento)
+    else:
+        result = dia.saldo()
+
+    return round(result / cotizacion, 2)
--- pytests/diario/templatetags/test_historicos.py
+++ pytests/diario/templatetags/test_historicos.py
@@ -1,4 +1,5 @@
-from diario.templatetags.historicos import cap_historico, saldo_en_moneda
+from diario.templatetags.historicos import cap_historico, saldo_en_moneda, saldo
+from diario.utils.utils_saldo import saldo_general_historico
 from utils.numeros import float_format
 
 
@@ -24,3 +25,63 @@ class TestSaldoHistoricoEnMoneda:
         assert \
             saldo_en_moneda(cuenta_con_saldo, dolar, None) == \
             float_format(cuenta_con_saldo.saldo(moneda=dolar, compra=False))
+
+
+class TestSaldo:
+    def test_devuelve_saldo_general(self, entrada, salida, salida_posterior):
+        assert saldo() == salida_posterior.dia.saldo()
+
+    def test_si_recibe_dia_devuelve_saldo_general_al_dia_recibido(self, dia, entrada, salida, salida_posterior):
+        assert saldo(dia=dia) == dia.saldo()
+
+    def test_si_recibe_movimiento_devuelve_saldo_general_al_momento_del_movimiento(
+            self, entrada, salida, salida_posterior):
+        assert saldo(movimiento=entrada) == saldo_general_historico(entrada)
+
+    def test_si_recibe_dia_y_movimiento_prioriza_movimiento(self, entrada, salida, salida_posterior):
+        assert saldo(dia=salida_posterior.dia, movimiento=entrada) == saldo_general_historico(entrada)
+
+    def test_si_recibe_moneda_devuelve_saldo_general_en_moneda_recibida(
+            self, entrada, salida, salida_posterior, dolar):
+        hoy = salida_posterior.dia
+        assert saldo(moneda=dolar) == round(hoy.saldo() / dolar.cotizacion_al(hoy.fecha, compra=False), 2)
+
+    def test_si_recibe_cuenta_devuelve_saldo_de_la_cuenta(
+            self, cuenta, entrada, entrada_otra_cuenta, salida_posterior):
+        assert saldo(cuenta=cuenta) == cuenta.saldo()
+
+    def test_si_recibe_cuenta_y_dia_devuelve_saldo_de_la_cuenta_al_dia_recibido(
+            self, cuenta, dia, entrada, entrada_otra_cuenta, salida_posterior):
+        assert saldo(cuenta=cuenta, dia=dia) == cuenta.saldo(dia=dia)
+
+    def test_si_recibe_cuenta_y_movimiento_devuelve_saldo_de_la_cuenta_al_momento_del_movimiento(
+            self, cuenta, entrada, salida, entrada_otra_cuenta, salida_posterior):
+        assert saldo(cuenta=cuenta, movimiento=entrada) == cuenta.saldo(movimiento=entrada)
+
+    def test_si_recibe_cuenta_y_moneda_devuelve_saldo_de_cuenta_en_moneda_recibida(
+            self, cuenta, entrada, entrada_otra_cuenta, salida_posterior, dolar):
+        assert saldo(cuenta=cuenta, moneda=dolar) == cuenta.saldo(moneda=dolar)
+
+    def test_si_recibe_cuenta_y_no_moneda_devuelve_saldo_en_moneda_de_la_cuenta(
+            self, dolar, cuenta_en_dolares, entrada_en_dolares, salida_en_dolares):
+        assert saldo(cuenta=cuenta_en_dolares) == cuenta_en_dolares.saldo(moneda=dolar)
+
+    def test_si_recibe_titular_devuelve_capital_del_titular(
+            self, titular, entrada, entrada_cuenta_ajena, salida_posterior):
+        assert saldo(titular=titular) == titular.capital()
+
+    def test_si_recibe_titular_y_dia_devuelve_capital_del_titular_al_dia_recibido(
+            self, dia, titular, entrada, entrada_cuenta_ajena, salida_posterior):
+        assert saldo(titular=titular, dia=dia) == titular.capital(dia=dia)
+
+    def test_si_recibe_titular_y_movimiento_devuelve_capital_del_titular_al_momento_del_movimiento(
+            self, titular, entrada, salida, entrada_cuenta_ajena, salida_posterior):
+        assert saldo(titular=titular, movimiento=entrada) == titular.capital(movimiento=entrada)
+
+    def test_si_recibe_titular_y_cuenta_prioriza_cuenta(
+            self, titular, cuenta_ajena, entrada, salida, entrada_cuenta_ajena, entrada_posterior_cuenta_ajena):
+        assert saldo(titular=titular, cuenta=cuenta_ajena) == cuenta_ajena.saldo()
+
+    def test_si_recibe_titular_y_moneda_devuelve_capital_en_moneda_recibida(
+            self, titular, entrada, salida, salida_posterior, dolar):
+        assert saldo(titular=titular, moneda=dolar) == round(titular.capital() / dolar.cotizacion, 2)
