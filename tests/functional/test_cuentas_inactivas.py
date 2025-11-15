88518c6244ad16eee63813456c33e433528a560e
Hernando Tejedor en arch <htejedor@gmail.com>
2025-11-15 11:08:23 -0300

    v2.4.6.0 No se muestran movimientos en página de cuentas inactivas

--- tests/functional/test_cuentas_inactivas.py
+++ tests/functional/test_cuentas_inactivas.py
@@ -1,6 +1,8 @@
 from django.urls import reverse
 from selenium.webdriver.common.by import By
 
+from diario.models import Movimiento
+
 
 def test_permite_activar_cuentas_inactivas(browser, cuenta_inactiva, cuenta):
     # En la página de cuentas inactivas, al lado de cada cuenta hay una opción "A"
@@ -34,3 +36,18 @@ def test_muestra_cuenta_madre_antes_de_subcuenta_inactiva(browser, cuenta_acumul
     cuentas = browser.encontrar_elementos("class_link_cuenta")
     nombres_cuenta = [x.text for x in cuentas]
     assert nombres_cuenta == [cuenta_acumulativa_saldo_0.nombre, sc1.nombre, sc3.nombre]
+
+
+def test_no_muestra_dias_ni_movimientos_excepto_en_detalle_de_cuenta_inactiva(browser, cuenta, entrada, salida):
+    # En la página de cuentas inactivas, por razones de rendimiento, no se muestran los movimientos
+    Movimiento.crear("puesta en cero", cuenta.saldo(), cta_salida=cuenta)
+    cuenta.refresh_from_db()
+    cuenta.activa = False
+    cuenta.clean_save()
+
+    browser.ir_a_pag(reverse("ctas_inactivas"))
+    browser.no_encontrar_elemento("id_section_movimientos")
+
+    # Sí se muestran los movimientos si vamos al detalle de cualquier cuenta inactiva
+    browser.ir_a_pag(cuenta.get_absolute_url())
+    browser.encontrar_elemento("id_section_movimientos")
